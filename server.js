5d7238ad2938bda3ba8ca0d830bb9de65db950a9
Andrew <andrew.vinocur@gmail.com>
2017-01-23 15:56:21 -0800

    can now view all users and save new users

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var db = require('./models');
+var User = require('./models/user.js');
 
 
 app.use(bodyParser.urlencoded({ extended: true}));
@@ -13,11 +14,35 @@ app.use(function(req, res, next) {
   next();
 });
 
-//Allows me to use static files from these folders?
+//Allows me to use static files from these folders
 app.use(express.static(__dirname + '/public'));
 app.use(express.static(__dirname + '/node_modules'));
 
+app.get('/users', function(req, res){
+  User.find({}).exec(function(err, users){
+    if (err) {
+      res.send(err);
+    } else {
+      res.send({data:users}, 200);
+    }
+  });
+});
 
+app.post('/users', function(req, res){
+  var newUser = new User({
+    email: req.body.email,
+    username: req.body.username,
+    trip: req.body.userBudget
+  });
+
+  newUser.save(function(err, user){
+    if (err) {
+      res.send(err);
+    } else {
+      res.send(user);
+    };
+  });
+});
 
 
 
